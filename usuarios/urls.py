f4edf816a61a0afc06d7e9ba27cb2898b2a9084b
20191230033 <ferreira.raquel@academico.ifpb.edu.br>
2022-08-17 14:13:15 -0300

    configurações de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(
         template_name='usuarios/form.html'
     ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
